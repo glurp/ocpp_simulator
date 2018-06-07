c2750594572947fc486570244492962b74096cdd
Regis d'Aubarede <regis.aubarede@gmail.com>
2018-06-07 19:42:11 +0200

    auto stop

--- ocpp16/ws_sender.rb
+++ ocpp16/ws_sender.rb
@@ -3,13 +3,19 @@
 # wssender.rb : connect to remote websocket serveur, send a text message and wait...
 #
 # Exemple:
-#   > ruby wssender.rb 10.203.76.160 3400 '[2,0,"heartbeat",{}]'
+#   > ruby ws_sender.rb ws://10.203.76.160:3400/ddd '[2,0,"heartbeat",{}]'
+#   > ruby ws_sender.rb --stop ws://10.203.76.160:3400/ddd '[2,0,"heartbeat",{}]' '[2,0,"heartbeat",{}]' '[2,0,"heartbeat",{}]' '[2,0,"heartbeat",{}]'
 #################################################################################
 require 'em-websocket-client'
 
 options = {}
+autostop=false
+if ARGV.first=="--stop"
+ autostop=true
+ ARGV.shift
+end
 if ARGV.size<2
-  puts "Usage : > #{$0} remote-host remote-port  data..." 
+  puts "Usage : > #{$0} [--stop] remote-host remote-port  data..." 
   exit(1)
 end
 url = ARGV.shift
@@ -19,9 +25,9 @@ EventMachine.run {
   puts "Connecting to #{url}..."
   ws = EventMachine::WebSocketClient.connect(url)
   ws.callback { 
-    p "connected"; 
-    mess.each_with_index {|m,ii| EM.add_timer(ii+1) {  p "sending #{m} ..." ; ws.send_msg(String.new(m)) } }
+    mess.each_with_index {|m,ii|  p "sending #{m} ..." ; ws.send_msg(String.new(m)) }
   }
   ws.disconnect { p "closed by client" ;  EM::stop_event_loop } 
   ws.stream { |msg| p "received #{msg}" }  
+  ( EM.add_timer(3+1 * mess.size) { exit(0) } ) if autostop
 }
